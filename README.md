ee0eb3116c76a4eae0c6de1c13313968a015254d
Anton Shevchuk <antonshevchuk@gmail.com>
2022-10-06 17:15:52 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# wme
-Helper GreasyFork script for Waze Map Editor
+# WME
+Helper class for Greasy Fork plugins for Waze Map Editor
+
+## Methods
+* `WME.getVenues(except = [])` – return Array of venues, except some categories
+* `WME.getSegments(except = [])` – return Array of segments, except some road types
+* `WME.getSelected()` – return Array of the selected **models**, which you can edit(!)
+* `WME.getSelectedVenue()` – return **model** of the selected venue, which you can edit(!)
+* `WME.getSelectedVenues()` – return Array of the selected venues **models**, which you can edit(!)
+* `WME.getSelectedSegment()` – return **model** of the selected segment, which you can edit(!)
+* `WME.getSelectedSegments()` – return Array of the selected segments **models**, which you can edit(!)
+* `WME.getSelectedNode()` – return **model** of the selected node, which you can edit(!), or null
+* `WME.getSelectedNodes()` – return Array of the selected nodes **models**, which you can edit(!)
---
+++ WME.user.js
@@ -0,0 +1,121 @@
+// ==UserScript==
+// @name         WME
+// @version      0.0.1
+// @description  Helper class for Greasy Fork plugins for Waze Map Editor
+// @license      MIT License
+// @author       Anton Shevchuk
+// @namespace    https://greasyfork.org/users/227648-anton-shevchuk
+// @supportURL   https://github.com/AntonShevchuk/wme-base/issues
+// @match        https://*.waze.com/editor*
+// @match        https://*.waze.com/*/editor*
+// @exclude      https://*.waze.com/user/editor*
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://anton.shevchuk.name&size=64
+// @grant        none
+// ==/UserScript==
+
+/* jshint esversion: 8 */
+
+/* global jQuery, W */
+
+class WME {
+  /**
+   * Get all available POI except selected categories
+   * @param {Array} except
+   * @return {Array}
+   */
+  static getVenues (except = []) {
+    let selected = W.model.venues.getObjectArray()
+    selected = selected.filter((el) => el.isGeometryEditable())
+    // filter by main category
+    if (except.length) {
+      selected = selected.filter(model => except.indexOf(model.getMainCategory()) === -1)
+    }
+    return selected
+  }
+
+  /**
+   * Get all available segments except selected road types
+   * @param {Array} except
+   * @return {Array}
+   */
+  static getSegments (except = []) {
+    let selected = W.model.segments.getObjectArray()
+    selected = selected.filter((el) => el.isGeometryEditable())
+    // filter by road type
+    if (except.length) {
+      selected = selected.filter(segment => except.indexOf(segment.getRoadType()) === -1)
+    }
+    return selected
+  }
+
+  /**
+   * Get selected features which you can(!) edit
+   * @returns {Array}
+   */
+  static getSelected () {
+    if (!W.selectionManager.hasSelectedFeatures()) {
+      return []
+    }
+    let selected
+    selected = W.selectionManager.getSelectedFeatures().map((x) => x.model)
+    selected = selected.filter((el) => el.isGeometryEditable())
+    return selected
+  }
+
+  /**
+   * Get selected Area POI(s)
+   * @return {Array}
+   */
+  static getSelectedVenues () {
+    return WME.getSelected().filter((el) => el.type === 'venue')
+  }
+
+  /**
+   * Get selected Area POI
+   * @return {Object|null}
+   */
+  static getSelectedVenue () {
+    if (WME.getSelectedVenues().length) {
+      return WME.getSelectedVenues()[0]
+    }
+    return null
+  }
+
+  /**
+   * Get selected Segments
+   * @return {Array}
+   */
+  static getSelectedSegments () {
+    return WME.getSelected().filter((el) => el.type === 'segment')
+  }
+
+  /**
+   * Get selected Segment
+   * @return {Object|null}
+   */
+  static getSelectedSegment () {
+    if (WME.getSelectedSegments().length) {
+      return WME.getSelectedSegments()[0]
+    }
+    return null
+  }
+
+  /**
+   * Get selected Nodes
+   * @return {Object}
+   */
+  static getSelectedNodes () {
+    return WME.getSelected().filter((el) => el.type === 'node')
+  }
+
+  /**
+   * Get selected Node
+   * @return {Object|null}
+   */
+  static getSelectedNode () {
+    if (WME.getSelectedNodes().length) {
+      return WME.getSelectedNodes()[0]
+    }
+    return null
+  }
+}
